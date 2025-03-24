76d27e3784a8fc77d1e5a4eb92d9843ae5fd6581
RizwanQubitars <rizwan@qubitars.com>
2025-03-24 21:00:57 +0500

    latest push

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// eslint-disable-next-line no-undef
 const express = require('express');
 const app = express();
 const PORT = 5000;
 
 // Simple Hello World API
 app.get('/', (req, res) => {
+    // const name  = "rizwan ali";
   res.send('Hello, World!');
 });
 
